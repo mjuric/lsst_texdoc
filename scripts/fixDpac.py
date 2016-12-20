a97bcdc7857e17aafcbdbffd93116a0dbed37f7f
William O'Mullane <womullan@sciops.esa.int>
2016-12-20 22:58:29 +0100

    small update to GBU

---
+++ scripts/fixDpac.py
@@ -0,0 +1,39 @@
+#python
+#Take gaia tex file and put in lsst ..
+import os
+import sys
+import shutil
+import fileinput
+
+
+
+
+def doFile(inFile ):
+	"This strips the dpac  tex an makes an lsst one "
+	count =0
+	f=inFile 
+	of = str(inFile) + ".old"
+	shutil.copy(f,of)
+	print "Processing " + of  +" "+ f 
+	fin = open (of,'r')  	
+	fout = open (f,'w')  	
+	for line in fin :
+		count= count + 1
+		line=line.replace("DpacDU","LSSTDU");
+		line=line.replace("dpacDU","LSSTDU");
+		line=line.replace("DpacD","D");
+		line=line.replace("dpacD","d");
+		line=line.replace("dpac","lsst");
+		fout.write(line);
+	fout.close()
+	fin.close()
+
+	print str(inFile) + " ...." + str(count) + " lines \n"
+	return;
+ # End DoDir
+
+
+
+
+### MAIN 
+doFile(sys.argv[1])
